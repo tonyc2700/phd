a0fe4819799def0200cad9792dbd5456667a63d7
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2015-04-14 14:40:54 +0100

    regression stuff

--- eval-missing/eval/imp.R
+++ eval-missing/eval/imp.R
@@ -1,6 +1,7 @@
 library(mice)
 library(VIM)
 library(lavaan)
+library(lme4)
 
 abdn <- read.csv("abdn.csv")
 abdn$matsm <- as.factor(abdn$matsm)
@@ -19,7 +20,7 @@ regComplete <- curve(abdnComplete, model)
 
 imp1 <- mice(abdn, print=FALSE)
 # fit1 <- with(imp1, curve(imp1, model))
-fit1 <- with(imp1, lm(Z_CRL_our ~ Z_BWT_ICH + z_BPD_our))
+fit1 <- with(imp1, lmer(Z_CRL_our ~ Z_BWT_ICH + z_BPD_our))
 regImp1 <- pool(fit1)
 
 regImp1Test <- round(summary(pool(fit1)),3)
