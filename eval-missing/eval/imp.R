689c42f904000911e5a8d2200c881ab865d85222
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2015-04-16 09:53:44 +0100

    trying out linear models

--- eval-missing/eval/imp.R
+++ eval-missing/eval/imp.R
@@ -7,7 +7,7 @@ abdn <- read.csv("abdn.csv")
 abdn$matsm <- as.factor(abdn$matsm)
 abdn$Quintile_SIMD_2006 <- as.factor(abdn$Quintile_SIMD_2006)
 
-source("eval.R")
+source("art.R")
 
 # Model will be used by all the regressions 
 model <- ' trajectory =~ Z_CRL_our + Z_BWT_ICH + z_BPD_our '
@@ -20,10 +20,13 @@ regComplete <- curve(abdnComplete, model)
 
 imp1 <- mice(abdn, print=FALSE)
 # fit1 <- with(imp1, curve(imp1, model))
-fit1 <- with(imp1, lmer(Z_CRL_our ~ Z_BWT_ICH + z_BPD_our))
+fit1 <- with(imp1, lm(Z_CRL_our ~ Z_BWT_ICH + z_BPD_our))
 regImp1 <- pool(fit1)
 
 regImp1Test <- round(summary(pool(fit1)),3)
 
 
 
+# test.model <- lm(Z_BWT_ICH ~ Z_CRL_our +z_BPD_our +Quintile_SIMD_2006 + matasev +matsm + bsexmf24, data=abdnComplete) 
+
+# lm(Z_BWT_ICH ~ Z_CRL_our +z_BPD_our, data=abdn)
